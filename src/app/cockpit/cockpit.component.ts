1b69f09446e8a4ec31aa0ed3e063b8f20ef36388
Jitgitbit <rowowolf@gmail.com>
2020-05-06 19:56:39 +0200

    Getting Access to the Template & DOM with @ViewChild

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 // import { EventEmitter } from 'protractor';
 
 @Component({
@@ -10,7 +10,8 @@ export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter <{serverName: string, serverContent: string}>();
   @Output('bpCreated') blueprintCreated = new EventEmitter <{serverName: string, serverContent: string}>();
   // newServerName = '';
-  newServerContent = '';
+  // newServerContent = '';
+  @ViewChild('serverContentInput', {static: true}) serverContentInput: ElementRef;
 
   constructor() { }
 
@@ -19,15 +20,17 @@ export class CockpitComponent implements OnInit {
  
   onAddServer(serverNameInput: HTMLInputElement) {
     console.log(`serverNameInput.value is:`, serverNameInput.value);
+    console.log(`this.serverContentInput is:`, this.serverContentInput);
     this.serverCreated.emit({
       serverName: serverNameInput.value, 
-      serverContent: this.newServerContent
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
   onAddBlueprint(serverNameInput: HTMLInputElement) {
+    // this.serverContentInput.nativeElement.value = 'SomethingSomething'                      YOU SHOULD NOT ACCESS THE DOM LIKE THIS !!!
     this.blueprintCreated.emit({
       serverName: serverNameInput.value, 
-      serverContent: this.newServerContent
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
 }
